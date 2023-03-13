e3a2b8b50beb3b1b0663efb533f559f5bff2a3ed
Neilson Venute <neilsonvenute@gmail.com>
2023-03-13 20:17:12 -0300

    Configurado o tratamento de erros e habilitado o salvar com o http post

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,10 @@
+import { Course } from './../model/course';
+import { tap } from 'rxjs';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -9,14 +14,27 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CourseFormComponent {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result),
+      (error) => this.onError()
+    );
+  }
 
   onCancel() {}
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar curso','',{duration: 5000})
+  }
 }
