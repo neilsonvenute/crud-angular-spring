cf311e450e5f277c74b443f2f05d16a3dc2842d8
Neilson Venute <neilsonvenute@gmail.com>
2023-03-13 21:00:01 -0300

    Criados o método de cancelar e voltar

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,9 @@
-import { Course } from './../model/course';
-import { tap } from 'rxjs';
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -17,7 +16,8 @@ export class CourseFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -27,12 +27,19 @@ export class CourseFormComponent {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe({
-      next: (result) => console.log(result),
+      next: (result) => this.onSuccess(),
       error: (error) => this.onError(),
     });
   }
 
-  onCancel() {}
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso', '', { duration: 5000 });
+    this.onCancel();
+  }
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
